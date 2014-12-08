46b051dc9691bb5f0d0a8344cecc5f386ba49e49
Mike Chelen <michael.chelen@gmail.com>
2014-12-08 03:39:38 -0500

    cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,96 +1,59 @@
-var yaml = require('js-yaml');
 var fs   = require('fs');
+var yaml = require('js-yaml');
 var yamlFront = require('yaml-front-matter');
-//curl = require('node-curl');
-var projectimages = require('./projectimages.json');
-//var projectimages = "";
+var projectimages = require('./img/projectimages.json');
+
 
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
+    curl: projectimages,
+    imagemin: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        optimizationLevel: 7,
+        progressive: true
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      projects: {                         // Another target
+      files: [{
+        expand: true,                  // Enable dynamic expansion
+        cwd: 'img/projects',                   // Src matches are relative to this path
+        src: ['*'],   // Actual patterns to match
+        dest: 'img/projects'                  // Destination path prefix
+      }]
       }
-    },
-    'curl-dir': {
-      'tmp/foo': projectimages
     }
   });
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-curl');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', []);
   
-  grunt.registerTask('foo', 'bar', function(arg) {
-  
-//  var filepath = "projects/_posts/2014-02-01-csv-js.md";
-  var filepath = "2014-02-01-csv-js.md";
-
-//    var filepath = "test.yml";
-
-/*
-// Get document, or throw exception on error
-try {
-  var doc = yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
-  console.log(doc);
-} catch (e) {
-  console.log(e);
-}
-*/
+  grunt.registerTask('projectimg', 'generate json list of project images', function(arg) {
+    // reads yaml frontmatter from each jekyll post in the projects folder
+    // saves output as img/projectimages.json
 
     var dirpath = "projects/_posts";
-
     var filenames = fs.readdirSync(dirpath);
-
-    var imageurls = [];
+    var imageurls = {};
 
     filenames.map(function(filename){
-      var fileyaml = yamlFront.loadFront(fs.readFileSync(dirpath + "/" + filename, 'utf8'));
-      if (fileyaml.imageurl){ // check if imageurl exists
-//      console.log(fileyaml.imageurl);
-      if (fileyaml.imageurl.indexOf("http") >= 0 ){
-            imageurls[imageurls.length] = fileyaml.imageurl;
-            }
+      // read frontmatter
+      var fileyaml = yamlFront.loadFront(fs.readFileSync(dirpath + "/" + filename, 'utf8')); 
+      // check if imageurl exists
+      if (fileyaml.imageurl){ 
+        // check if external image url
+        if (fileyaml.imageurl.indexOf("http") >= 0 ){
+            imageurls["img/projects/"+fileyaml.slug] = fileyaml.imageurl;
+        }
       }
     });
 
-//    var results = yamlFront.loadFront(fs.readFileSync(filepath, 'utf8'));
-
-//    console.log(results);
-
-//    console.log(yaml.safeDump(imageurls));
-    
-
-    console.log(imageurls);
-    
-//    console.log(JSON.stringify(imageurls));
-
-    fs.writeFileSync("projectimages.json", JSON.stringify(imageurls)); //write image urls to file
-
-
-//    console.log(dirs);
-
-//    var foo = grunt.file.readYAML(filepath);
-
-  
-//    var msg = 'baz';
-//    console.log(foo);
-
-
-
-
-    
-    
+    fs.writeFileSync("img/projectimages.json", JSON.stringify(imageurls)); //write to file
   });
 
 };
